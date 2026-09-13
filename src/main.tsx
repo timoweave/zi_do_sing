d82f0c4546ff51e3153c357f0777dae94dd40574
Timothy Shiu <timoweave@gmail.com>
2026-09-12 20:50:50 -0700

    add rounded chinese fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import "./index.css"
-import App from "./App.tsx"
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import '@fontsource-variable/noto-sans';
+import '@fontsource-variable/noto-serif';
+import '@fontsource/huninn';
+import 'cn-fontsource-source-han-serif-sc-vf/font.css';
+import 'cn-fontsource-lxgw-wen-kai-screen-r/font.css';
+import 'cn-fontsource-975-maru-sc-bold/font.css';
+import 'cn-fontsource-975-maru-sc-regular/font.css';
+import 'cn-fontsource-975-maru-sc-medium-regular/font.css';
+
+// import '@wcex/cn-fontsource';
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
